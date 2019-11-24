eec00fca8671a1470d88c2950aeb2aa543ccfbcf
Raphael Vidal <raphael.r_vidal@hotmail.com>
2019-11-24 20:10:21 -0300

    criação app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'sobre',
+    loadChildren: () => import('./sobre/sobre.module').then( m => m.SobrePageModule)
   }
 ];
 
